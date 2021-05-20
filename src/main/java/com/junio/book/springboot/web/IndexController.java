8ded0323229d269c2738e2b5ba183e1ab6072ba0
HJS <junio5188@gmail.com>
2021-05-20 23:47:48 +0900

    3일차 끝

--- src/main/java/com/junio/book/springboot/web/IndexController.java
+++ src/main/java/com/junio/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.junio.book.springboot.web;
 
+import com.junio.book.springboot.config.auth.dto.SessionUser;
 import com.junio.book.springboot.service.posts.PostsService;
 import com.junio.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -28,7 +36,6 @@ public class IndexController {
 
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
-        System.out.println("IndexController::::::::::::::::::");
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
